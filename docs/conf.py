2ee8e1a07d6a19320768ecfcbd8e2359f7e0fc29
Alexandru Fikl <alexfikl@gmail.com>
2021-01-10 11:33:28 -0600

    add some empty files

---
+++ docs/conf.py
@@ -0,0 +1,92 @@
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+try:
+    # python >=3.8 only
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
+
+# {{{ project information
+
+m = metadata.metadata("h5pyckle")
+project = m["Name"]
+author = m["Author"]
+copyright = "2021 {}".format(author)
+version = m["Version"]
+release = version
+
+# }}}
+
+# {{{ general configuration
+
+# needed extensions
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+]
+
+try:
+    import sphinxcontrib.spelling       # noqa: F401
+    extensions.append("sphinxcontrib.spelling")
+except ImportError:
+    pass
+
+# extension for source files
+source_suffix = ".rst"
+# name of the main (master) document
+master_doc = "index"
+# min sphinx version (needed for `autodoc_type_aliases`)
+needs_sphinx = "3.3"
+# files to ignore
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+# highlighting
+pygments_style = "sphinx"
+
+# }}}
+
+# {{{ internationalization
+
+language = "en"
+
+# sphinxcontrib.spelling options
+spelling_lang = "en_US"
+tokenizer_lang = "en_US"
+spelling_word_list_filename = "wordlist_en.txt"
+
+# }}
+
+# {{{ output
+
+# html
+html_theme = "sphinx_rtd_theme"
+
+# }}}
+
+# {{{ extension settings
+
+autoclass_content = "class"
+autodoc_member_order = "bysource"
+autodoc_default_options = {
+    "show-inheritance": None,
+}
+
+autodoc_typehints = "description"
+
+# https://github.com/sphinx-doc/sphinx/issues/8127
+nitpick_ignore = [
+    ("py:class", "..")
+]
+
+# }}}
+
+# {{{ links
+
+intersphinx_mapping = {
+    "https://docs.python.org/3": None,
+    "https://numpy.org/doc/stable": None,
+    "https://docs.h5py.org/en/stable/": None,
+}
+
+# }}}
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. h5pyckle documentation master file, created by
+   sphinx-quickstart on Sun Jan 10 11:30:22 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to h5pyckle's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
